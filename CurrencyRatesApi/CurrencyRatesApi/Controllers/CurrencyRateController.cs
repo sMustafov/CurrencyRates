83b27c5367424a59cc3adb35a1f317b9b95c9679
Sevgin Mustafov <sevgin95@gmail.com>
2021-02-05 21:43:04 +0200

    Basic logic for loading xml

---
+++ CurrencyRatesApi/CurrencyRatesApi/Controllers/CurrencyRateController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Xml.Linq;
+
+namespace CurrencyRatesApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CurrencyRateController : ControllerBase
+    {
+        const string URLString = "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml";
+
+        private readonly ILogger<CurrencyRateController> logger;
+        private readonly List<CurrencyRate> currencyRateList;
+
+
+        public CurrencyRateController(ILogger<CurrencyRateController> logger)
+        {
+            this.logger = logger;
+            this.currencyRateList = new List<CurrencyRate>();
+        }
+
+        public CurrencyRate CurrencyRate { get; private set; }
+
+        [HttpGet]
+        public IEnumerable<CurrencyRate> Get()
+        {
+
+            XmlDocument myXmlDocument = new XmlDocument();
+            myXmlDocument.Load(URLString);
+
+            foreach (XmlNode nodes in myXmlDocument.DocumentElement.ChildNodes)
+            {
+                foreach (XmlNode node in nodes)
+                {
+                    if (node.Name == "Cube")
+                    {
+                        foreach (XmlNode childNode in node.ChildNodes)
+                        {
+                            CurrencyRate = new CurrencyRate
+                            {
+                                Currency = childNode.Attributes[0].Value,
+                                Rate = childNode.Attributes[1].Value
+                            };
+
+                            this.currencyRateList.Add(CurrencyRate);
+                        }
+                    }
+                }
+            }
+
+            return this.currencyRateList;
+        }
+    }
+}
---
+++ CurrencyRatesApi/CurrencyRatesApi/CurrencyRate.cs
@@ -0,0 +1,8 @@
+namespace CurrencyRatesApi
+{
+    public class CurrencyRate
+    {
+        public string Currency { get; set; }
+        public string Rate { get; set; }
+    }
+}
